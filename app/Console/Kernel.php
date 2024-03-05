de1333e88a2b9f3a8ba82508ccf2e828a08bec5f
gptautopost <contatoautopost@gmail.com>
2024-03-05 12:25:12 -0300

    fix:command updated

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         $this->load(__DIR__.'/Commands');
 
-        require base_path('routes/console.php');
+        require base_path('routes/console.php');    
     }
 
     
