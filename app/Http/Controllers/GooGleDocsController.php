8834d8385401f9599294167ee4d356860c5365fe
cosmosgc <viniciusbretasavezani@gmail.com>
2024-03-22 10:26:16 -0300

    Select all + gdrive button

--- app/Http/Controllers/GooGleDocsController.php
+++ app/Http/Controllers/GooGleDocsController.php
@@ -12,41 +12,35 @@ class GooGleDocsController extends Controller
 {
     //
 
-protected $googleDocsService;
-protected $postService;
+    protected $googleDocsService;
+    protected $postService;
 
-public function __construct(PostFileService $googleDocs)
-{
-    $this->googleDocsService=$googleDocs;
-}
-
-public function insertDocOnDB(Request $request){
-
-
-    if (!$request->session()->has('google_access_token')) {
-        // Se o token de acesso não estiver presente, redirecione para o processo de autenticação do Google
-        return redirect()->route('google.redirect');
-    }
-    if($request->has('google_docs')){
-        $doc_content=$this->googleDocsService->importGoogleDocs($request->google_docs);
-        $content=Wp_post_content::where('theme',$request->title)->update(['post_content'=>$doc_content]);
-        return redirect()->route('dashboard.DocumentImported');
-
-
-    }
-}
-
-public function createDocFromDb(Request $request){
-
-    if (!$request->session()->has('google_access_token')) {
-        // Se o token de acesso não estiver presente, redirecione para o processo de autenticação do Google
-        return redirect()->route('google.redirect');
+    public function __construct(PostFileService $googleDocs)
+    {
+        $this->googleDocsService=$googleDocs;
     }
 
-    $content=Wp_post_content::where('theme',$request->title)->get();
-    $doc_created=$this->googleDocsService->createAndPopulateGoogleDoc($content[0]->theme,$content[0]->post_content,$request->folder_id);
-    return $doc_created;
+    public function insertDocOnDB(Request $request){
+        if (!$request->session()->has('google_access_token')) {
+            // Se o token de acesso não estiver presente, redirecione para o processo de autenticação do Google
+            return redirect()->route('google.redirect');
+        }
+        if($request->has('google_docs')){
+            $doc_content=$this->googleDocsService->importGoogleDocs($request->google_docs);
+            $content=Wp_post_content::where('theme',$request->title)->update(['post_content'=>$doc_content]);
+            return redirect()->route('dashboard.DocumentImported');
+        }
+    }
 
+    public function createDocFromDb(Request $request){
 
-}
+        if (!$request->session()->has('google_access_token')) {
+            // Se o token de acesso não estiver presente, redirecione para o processo de autenticação do Google
+            return redirect()->route('google.redirect');
+        }
+        //title não é unique key
+        $content=Wp_post_content::where('theme',$request->title)->get();
+        $doc_created=$this->googleDocsService->createAndPopulateGoogleDoc($content[0]->theme,$content[0]->post_content,$request->folder_id);
+        return $doc_created;
+    }
 }
--- resources/views/dashboard/SubmitPosts.blade.php
+++ resources/views/dashboard/SubmitPosts.blade.php
@@ -120,7 +120,7 @@
               <thead>
 
                 <tr>
-                    <th>id</th>
+                    <th><input type="checkbox" id="selectAllCheckbox" onclick="selectAllCheckbox()"></th>
                   <th>Tema</th>
                   <th>Palavra-chave</th>
                   <th>Categoria</th>
@@ -170,6 +170,9 @@
                     <!-- Atualizar conteúdo Button with Font Awesome icon, alt attribute, and popover -->
                     <button class="btn btn-success update_content" data-toggle="popover" data-placement="top" title="Atualizar conteúdo" data-content="Clique para atualizar o conteúdo" onclick="open_modal(`{{$config->id}}`,`{{$config}}`)">
                     <i class="fas fa-sync-alt"></i>
+                    </button>
+                    <button class="btn btn-primary gdrive_doc" data-toggle="popover" data-placement="top" title="Criar doc" data-content="Clique para salvar em documento google drive" onclick="create_gdoc(`{{$config->id}}`,`{{$config}}`)">
+                    <i class="fab fa-google"></i>
                     </button>
 
                   </td>
@@ -222,6 +225,17 @@
     </div>
 
     <script>
+        function selectAllCheckbox(){
+            const selectAllCheckbox = document.getElementById('selectAllCheckbox');
+            const checkboxes = document.querySelectorAll('.form-check-input');
+            // Get the value of the select all checkbox
+            const isChecked = selectAllCheckbox.checked;
+
+            // Loop through all checkboxes and set their checked state
+            checkboxes.forEach(function(checkbox) {
+            checkbox.checked = isChecked;
+            });
+        }
         function getSelectedItems() {
           var checkboxes = document.querySelectorAll('.form-check-input');
           var selectedItems = [];
@@ -334,7 +348,7 @@
                     url_link_3: _url_link_3.value,
                     _token: csrfToken
                 };
-                console.log(data);
+                // console.log(data);
 
                 const updateQuery= await fetch('/update_config',{
                     method:'PUT',
@@ -371,7 +385,7 @@
 
                 if(updateQuery.ok){
                     alert('atualização feita com sucesso');
-                    console.log(updateQuery);
+                    // console.log(updateQuery);
                 }else{
                     alert('atualização falhou');
                 }
@@ -383,7 +397,7 @@
               function batch_generate(){
                 selected_items = getSelectedItems();
                 separatedData = separateThemesAndIDs(selected_items);
-                console.log(separatedData.themes, separatedData.ids);
+                // console.log(separatedData.themes, separatedData.ids);
                 //return;
                 generate_post(separatedData.themes, separatedData.ids);
             }
@@ -425,7 +439,7 @@
                     if (id !== undefined) {
                         body.id = id;
                     }
-                    console.log(body);
+                    // console.log(body);
                     try {
                         const query = await fetch('/gpt_query', {
                             method: 'POST',
@@ -482,8 +496,8 @@
                         _token: csrfToken
                     };
 
-                console.log(data);
-                //return;
+                // console.log(data);
+                // return;
                 try {
                     const query = await fetch('/post_content', {
                         method: 'POST',
@@ -494,6 +508,14 @@
                     if (query.ok) {
                         const data = await query.json();
                         console.log(data);
+
+                        //update_yoaust precisa de um request especifico com domain, keyword e id
+
+                        // const query_2 = await fetch('/update_yoaust', {
+                        //     method: 'POST',
+                        //     body: JSON.stringify(body),
+                        //     headers: { "Content-Type": "application/json" }
+                        // });
                     } else {
                         console.error("Fetch failed with status:", query.status);
                     }
@@ -501,9 +523,31 @@
                     console.error("Fetch error:", error);
                 }
             }
+            async function create_gdoc(theme){
+                //folderlink está hardcoded
+                let folderLink='https://drive.google.com/drive/folders/1iGQA7TFu1f7mp3r0SY7MTNDqPF72Ucl8?usp=sharing'
+                const folderId=folderLink.split('/folders/');
+                const folder=folderId[1];
+                const realForlderId=folder.split('?usp=sharing');
+                let body = {
+                            title: theme,
+                            folder_id:realForlderId[0],
+                            _token: csrfToken
+                        }
+                        const query = await fetch('/create_doc', {
+                            method: 'POST',
+                            body: JSON.stringify(body),
+                            headers: { "Content-Type": "application/json" }
+                        });
+
+                const response=await query.json()
+
+                console.log(response);
+                return response;
+            }
             function open_modal(i = 0, data = null) {
                 let parsedData = JSON.parse(data);
-                console.log(parsedData);
+                // console.log(parsedData);
 
                 modal.classList.add('open_editor_modal');
 
@@ -601,8 +645,6 @@
     </div>
 
     <script>
-
-      //const deleteButton= document.querySelector(".delete_config");
       const createDocButton= document.querySelector(".create_doc");
       const postContent=document.querySelector('.post-content')
       const languages=document.getElementById("languages");
@@ -610,7 +652,6 @@
       const writing_tone=document.getElementById('writing_tone');
       const sections=document.getElementById('sections');
       const paragraphs= document.getElementById('paragraphs');
-      //const csrfToken = document.head.querySelector('meta[name="csrf-token"]').content;
       const user_id= document.querySelector('.user_id')
 
 
@@ -620,243 +661,133 @@
       document.addEventListener('DOMContentLoaded', function () {
       const generateContentButtons = document.querySelectorAll('.create_content');
       const postButton= document.querySelectorAll(".post_wp");
-      //const generateButton=document.querySelector('.generateContentBtn');
       let data_id;
       let Theme;
 
+    const container = document.querySelectorAll('.container');
 
-      // generateContentButtons.forEach((button,i) => {
+    postButton.forEach((button,i)=>{
+        // Encontra o contêiner mais próximo ao botão clicado
+        const configId = id[i].getAttribute('data-id');
+            // console.log('Config ID:', configId);
+        const loading= document.createElement('span');
 
-      //   button.addEventListener('click', function () {
-      //       //Não precisamos mais disso
-      //   //   const modal = document.querySelector('.modal');
-      //   //   modal.classList.add('show');
-      //   //   modal.style.display = 'block';
-      //   //   document.body.classList.add('modal-open');
+            loading.innerHTML='loading....'
+            button.addEventListener('click',async()=>{
+            button.insertAdjacentElement("beforebegin", loading);
+            const domain=document.querySelectorAll('.domain')[i];
+            const keyword=document.querySelectorAll('.keyword')[i]
+            console.log("Postando em: "+domain.innerText);
+            data = {
+                            id: configId,
+                            user_id: user_id.value,
+                            domain: domain.innerText,
+                            _token: csrfToken
+                        };
 
-      //   //     data_id = button.closest('.container').getAttribute('data-id');
-      //   //     Theme = button.closest('.container').querySelector('.theme').innerText;
-      //   //   });
+                // console.log(data);
+                try {
+                    const query = await fetch('/post_content', {
+                        method: 'POST',
+                        body: JSON.stringify(data),
+                        headers: { "Content-Type": "application/json" }
+                    });
 
-      //   });
+                    if (query.ok) {
+                        const data = await query.json();
+                        // console.log(data);
+                        try {
+                            body = {id: data.id,
+                                    domain: domain.innerText,
+                                    keyword: keyword.innerText,
+                                    _token: csrfToken}
+                            const query_2 = await fetch('/update_yoaust', {
+                                method: 'POST',
+                                body: JSON.stringify(body),
+                                headers: { "Content-Type": "application/json" }
+                            });
 
-
-//         generateButton.addEventListener('click',async ()=>{
-//           const loading=document.createElement('div');
-//           const loadingSVG = `
-//                     <svg width="40" height="40" viewbox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
-//             <circle cx="20" cy="20" fill="none" r="10" stroke="#000" stroke-width="2">
-//               <animate attributeName="r" from="8" to="20" dur="1.5s" begin="0s" repeatCount="indefinite"/>
-//               <animate attributeName="opacity" from="1" to="0" dur="1.5s" begin="0s" repeatCount="indefinite"/>
-//             </circle>
-//           <!--   <circle cx="20" cy="20" fill="#383a36" r="10"/> -->
-//           </svg>
-
-//           `
-//           loading.innerHTML=loadingSVG;
-
-//           const modalDialog = document.querySelector('.modal-dialog');
-//           modalDialog.appendChild(loading);
-
-//             let topics = [Theme];
-//             let body = {
-//                 topics: topics,
-//                 _token: csrfToken
-//             };
-//             console.log(body);
-//             try {
-//                 const query = await fetch('/gpt_query', {
-//                     method: 'POST',
-//                     body: JSON.stringify(body),
-//                     headers: { "Content-Type": "application/json" }
-//                 });
-
-
-
-//         // Remove o SVG de loading após a conclusão da query
-//         try {
-//           if(query.ok){
-//           Swal.fire({
-//             title: 'Post publicado com sucesso?',
-//             text: 'Do you want to continue',
-//             icon: 'success',
-//             confirmButtonText: 'continue'
-//           })
-//         }
-
-//         } catch (error) {
-//           Swal.fire({
-//             title: error,
-//             text: 'Quer continuar?',
-//             icon: 'error',
-//             confirmButtonText: 'continue'
-//           })
-//         }
-
-
-//         modalDialog.removeChild(loading);
-
-//         // Aqui você pode adicionar código para lidar com a resposta da query, se necessário
-//     } catch (error) {
-//         console.error('Ocorreu um erro:', error);
-//         Swal.fire({
-//             title: 'Error on the process',
-//             text: 'Do you want to continue',
-//             icon: 'error',
-//             confirmButtonText: 'continue'
-//           })
-//         // Se ocorrer um erro, é importante remover o SVG de loading para evitar confusão
-//         document.body.removeChild(loadingSVG);
-//     }
-// })
-
-
-
-const container = document.querySelectorAll('.container');
-
-postButton.forEach((button,i)=>{
-       // Encontra o contêiner mais próximo ao botão clicado
-      const configId = id[i].getAttribute('data-id'); // Find the td with class "config_id" and get its text content
-        console.log('Config ID:', configId);
-      const loading= document.createElement('span');
-
-        loading.innerHTML='loading....'
-        button.addEventListener('click',async()=>{
-          button.insertAdjacentElement("beforebegin", loading);
-          const domain=document.querySelectorAll('.domain')[i];
-          const keyword=document.querySelectorAll('.keyword')[i]
-          console.log("Postando em: "+domain.innerText);
-          data = {
-                        id: configId,
-                        user_id: user_id.value,
-                        domain: domain.innerText,
-                        _token: csrfToken
-                    };
-
-            console.log(data);
-            try {
-                const query = await fetch('/post_content', {
-                    method: 'POST',
-                    body: JSON.stringify(data),
-                    headers: { "Content-Type": "application/json" }
-                });
-
-                if (query.ok) {
-                    const data = await query.json();
-                    console.log(data);
-                    try {
-                        body = {id: data.id,
-                                domain: domain.innerText,
-                                keyword: keyword.innerText,
-                                _token: csrfToken}
-                                console.log(body);
-                        const query_2 = await fetch('/update_yoaust', {
-                            method: 'POST',
-                            body: JSON.stringify(body),
-                            headers: { "Content-Type": "application/json" }
-                        });
-
-                        if (query_2.ok) {
-                            const data_2 = await query_2.json();
-                            console.log(data_2);
-                            //fazer o query mais uma vez não resolveu o bug
-
-                            // const query_3 = await fetch('/update_yoaust', {
-                            //     method: 'POST',
-                            //     body: JSON.stringify(body),
-                            //     headers: { "Content-Type": "application/json" }
-                            // });
+                            if (query_2.ok) {
+                                const data_2 = await query_2.json();
+                                Swal.fire({
+                                    title: 'Post criado com sucesso no wordpress',
+                                    text: 'Continuar?',
+                                    icon: 'success',
+                                    confirmButtonText: 'continue'
+                                })
+                                loading.remove(this);
+                            } else {
+                                console.error("Second fetch failed with status:", query_2.status);
+                            }
+                        } catch (error_2) {
+                            console.error("Second fetch error:", error_2);
                             Swal.fire({
-                                title: 'Post criado com sucesso no wordpress',
+                                title: query.statusText,
                                 text: 'Continuar?',
-                                icon: 'success',
+                                icon: 'error',
                                 confirmButtonText: 'continue'
                             })
                             loading.remove(this);
-                        } else {
-                            console.error("Second fetch failed with status:", query_2.status);
                         }
-                    } catch (error_2) {
-                        console.error("Second fetch error:", error_2);
-                        Swal.fire({
-                            title: query.statusText,
-                            text: 'Continuar?',
-                            icon: 'error',
-                            confirmButtonText: 'continue'
-                        })
-                        loading.remove(this);
+                    } else {
+                        console.error("Fetch failed with status:", query.status);
                     }
-
-
-
-                } else {
-                    console.error("Fetch failed with status:", query.status);
+                } catch (error) {
+                    console.error("Fetch error:", error);
                 }
-            } catch (error) {
-                console.error("Fetch error:", error);
-            }
-
-
-
-
-
-
+            })
         })
-      })
-
-});
-
-
-
-// Fechar o modal ao clicar no botão Fechar
-document.querySelectorAll('[data-dismiss="modal"]').forEach(btn => {
-        btn.addEventListener('click', () => {
-        const modal = document.querySelector('.modal.show');
-        modal.classList.remove('show');
-        modal.style.display = 'none';
-        document.body.classList.remove('modal-open');
     });
-});
+
+    // Fechar o modal ao clicar no botão Fechar
+    document.querySelectorAll('[data-dismiss="modal"]').forEach(btn => {
+            btn.addEventListener('click', () => {
+            const modal = document.querySelector('.modal.show');
+            modal.classList.remove('show');
+            modal.style.display = 'none';
+            document.body.classList.remove('modal-open');
+        });
+    });
 
 
-const deletebutton= document.querySelectorAll(".delete_config")
+    const deletebutton= document.querySelectorAll(".delete_config")
 
 
 
 
-deletebutton.forEach((e,i)=>{
-  e.addEventListener('click',async ()=>{
-    data_id=document.querySelector(".container").getAttribute("data-id");
-    console.log(data_id);
-    const deletion_query= await fetch('/remove_config',{
-      method:'DELETE',
-      body:JSON.stringify({
-         id:data_id,
-        _token:csrfToken
-      }),
-      headers:{"Content-Type":"application/json"}
+    deletebutton.forEach((e,i)=>{
+    e.addEventListener('click',async ()=>{
+        data_id=document.querySelector(".container").getAttribute("data-id");
+        // console.log(data_id);
+        const deletion_query= await fetch('/remove_config',{
+        method:'DELETE',
+        body:JSON.stringify({
+            id:data_id,
+            _token:csrfToken
+        }),
+        headers:{"Content-Type":"application/json"}
+        })
+
+        if(deletion_query.ok){
+        Swal.fire({
+                title: 'Configuração removida com sucesso',
+                text: 'Do you want to continue',
+                icon: 'success',
+                confirmButtonText: 'continue'
+            })
+            location.reload()
+        }else{
+        Swal.fire({
+                title: 'Erro ao remover configuração',
+                text: 'Do you want to continue',
+                icon: 'error',
+                confirmButtonText: 'continue'
+            })
+
+            location.reload();
+        }
     })
-
-    if(deletion_query.ok){
-      Swal.fire({
-            title: 'Configuração removida com sucesso',
-            text: 'Do you want to continue',
-            icon: 'success',
-            confirmButtonText: 'continue'
-          })
-          location.reload()
-    }else{
-      Swal.fire({
-            title: 'Erro ao remover configuração',
-            text: 'Do you want to continue',
-            icon: 'error',
-            confirmButtonText: 'continue'
-          })
-
-          location.reload();
-    }
-  })
-})
+    })
 
 </script>
 
