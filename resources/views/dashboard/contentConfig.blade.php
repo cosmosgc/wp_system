4fe6ccdee26e30ae45b6bfc0e3f7dca62044ce8d
gptautopost <contatoautopost@gmail.com>
2024-04-01 13:58:21 -0300

    fix: reload updated

--- resources/views/dashboard/contentConfig.blade.php
+++ resources/views/dashboard/contentConfig.blade.php
@@ -278,7 +278,7 @@ $user=explode('+',base64_decode($valorCodificado));
                             confirmButtonText: 'continue'
                         }).then((result) => {
                             if (result.isConfirmed) {
-                                //window.location.reload();
+                                window.location.reload();
                             }
                         });
 
