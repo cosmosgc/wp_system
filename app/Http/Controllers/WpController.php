d56b7814a445e917981f9b103402b2e5eacfd278
cosmosgc <viniciusbretasavezani@gmail.com>
2024-04-05 12:13:56 -0300

    backend if logic changes to update yoast

--- app/Http/Controllers/WpController.php
+++ app/Http/Controllers/WpController.php
@@ -30,14 +30,14 @@ class WpController extends Controller
             }
 
             public function updateYoast(Request $request){
-                if(isset($request->post_id)){
+                if(isset($request->post_id) && !isset($request->keyword)){
                     $update_meta=$this->wpService->updateYoastRankScore($request->domain,($request->post_id),($request->keyword));
                     return response($update_meta);
                 }
                 else{
                     $image=Wp_post_content::find($request->id);
                     $update_meta=$this->wpService->updateYoastRankMath($image->domain,intval($request->post_id),isset($image->keyword)?$image->keyword:'placeholder');
-                    return response(200);
+                    return response($update_meta);
                 }
             }
 }
