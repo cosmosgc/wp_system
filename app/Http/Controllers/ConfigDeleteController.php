27fdbc10bd134b13552b6eb88747ec41a8c78519
Bruno Barreto <bruno-kk@hotmail.com>
2024-04-14 01:13:18 -0300

    fix: delete image from storage added

--- app/Http/Controllers/ConfigDeleteController.php
+++ app/Http/Controllers/ConfigDeleteController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Wp_post_content;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ConfigDeleteController extends Controller
 {
     //
     public function deleteConfig(Request $request){
         $delete_request =Wp_post_content::find($request->id);
+        if (Storage::disk('public')->exists($delete_request->post_image)) {
+            // Deleta o arquivo
+            Storage::disk('public')->delete($delete_request->post_image);
+             // Retorna verdadeiro se a exclusão for bem-sucedida
+        }
         $deletion=$delete_request->delete();
         return response()->json($deletion, 200);
     }
