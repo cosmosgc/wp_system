a882d84d0139431299f684592759f9782396143c
cosmosgc <viniciusbretasavezani@gmail.com>
2024-04-15 18:26:22 -0300

    add a check if exist the api key

--- app/Http/Controllers/GptController.php
+++ app/Http/Controllers/GptController.php
@@ -56,7 +56,7 @@ class GptController extends Controller
         $user=explode('+',base64_decode($valorCodificado));
         $editor=Editor::where('name',$user)->get();
         $token=Editor::find($editor[0]->id)->iaCredentials;
-        $Google_api_key=Drive_credential::all()->first()->api_key;
+        $Google_api_key = isset(Drive_credential::all()->first()->api_key) ? Drive_credential::all()->first()->api_key : false;
 
         $title = $post_title;
         $language = $token->language;
