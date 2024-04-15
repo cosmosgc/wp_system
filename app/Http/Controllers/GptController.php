8b4337facdfc2ee65aa4ced7117989e928520c09
cosmosgc <viniciusbretasavezani@gmail.com>
2024-04-15 17:42:13 -0300

    spacing typo

--- app/Http/Controllers/GptController.php
+++ app/Http/Controllers/GptController.php
@@ -337,8 +337,8 @@ class GptController extends Controller
     }
     // Pesquisar o primeiro link do youtube
     public function searchYouTubeAndGetURL($api_key,$query) {
-         if($api_key == '' || $api_key == null){
-              return '';
+        if($api_key == '' || $api_key == null){
+            return '';
         }
         $apiKey = $api_key;
         $apiEndpoint = 'https://www.googleapis.com/youtube/v3/search';
