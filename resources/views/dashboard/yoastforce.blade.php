8bf60c52fb167421afb686969b01bc4ac2ea58af
cosmosgc <cosmoskitsune@hotmail.com>
2024-05-02 15:26:00 -0300

    tentei remover mais duplicatas dos domains yoaust

--- resources/views/dashboard/yoastforce.blade.php
+++ resources/views/dashboard/yoastforce.blade.php
@@ -11,6 +11,7 @@
     $valorCodificado = request()->cookie('editor');
     $user = explode('+', base64_decode($valorCodificado));
     $post_configs = Editor::where('name', $user[0])->get();
+    $uniqueDomains = [];
 @endphp
 
 @section('content')
@@ -24,9 +25,14 @@
                 <input type="text" id="search-input" class="form-control" placeholder="Pesquisar por dominios">
             </div>
             <select id="domain-select" multiple class="form-control">
-                @foreach($credentials->unique('wp_domain') as $credential)
+            @foreach($credentials as $credential)
+                @if (!in_array($credential->wp_domain, $uniqueDomains))
                     <option value="{{$credential->wp_domain}}">{{$credential->wp_domain}}</option>
-                @endforeach
+                    @php
+                        $uniqueDomains[] = $credential->wp_domain;
+                    @endphp
+                @endif
+            @endforeach
             </select>
         </div>
     </div>
