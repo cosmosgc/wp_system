16625cfae392276f81b63b145b961623b9daa0dd
cosmosgc <cosmoskitsune@hotmail.com>
2024-05-03 16:31:47 -0300

    test3

--- resources/views/dashboard/yoastforce.blade.php
+++ resources/views/dashboard/yoastforce.blade.php
@@ -29,7 +29,7 @@
             </div>
             <select id="domain-select" multiple class="form-control">
             @foreach($credentials->unique('wp_domain')->sortBy('wp_domain') as $credential)
-                    <option value="{{$credential->wp_domain}}" >{{$credential->wp_domain}}</option>
+                <option value="{{ e($credential->wp_domain) }}">{{ e($credential->wp_domain) }}</option>
             @endforeach
             </select>
         </div>
