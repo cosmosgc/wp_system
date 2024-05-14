0ccd0867dc1865a1bf5b6e306fb7d6ac63b2c6cb
Bruno Barreto <bruno-kk@hotmail.com>
2024-05-14 19:07:26 -0300

    debug-5

--- app/Http/Controllers/CsvReaderController.php
+++ app/Http/Controllers/CsvReaderController.php
@@ -80,8 +80,8 @@ class CsvReaderController extends Controller
                      $userData=json_decode($teste);
                      if(!empty($dt['Imagem'])){
                         try {
-                            $addImage=$this->imageService->downloadImageFromGoogleDrive('',$userData);
-                            //dd($addImage);
+                            $test=$this->imageService->downloadImageFromGoogleDrive('',$userData);
+                            dd($test);
                         } catch (\Throwable $th) {
                             return $th;
                         }
